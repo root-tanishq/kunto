8f70a12d8f77b92539c6958f99ab8a890768c36e
⛩️kun <82044074+root-tanishq@users.noreply.github.com>
2022-08-30 21:03:21 +0530

    Add files via upload

---
+++ scripts/kuntool_Sublist3r.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# URL to Repository => https://github.com/aboul3la/Sublist3r
+# Author => Ahmed Aboul-Ela
+${SMK_SUDO} apt install git python python3 python-pip python3-pip -y 2>/dev/null
+git clone https://github.com/aboul3la/Sublist3r.git ${DIR}/Sublist3r
+${SMK_SUDO} python -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
+${SMK_SUDO} python3 -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
+${SMK_SUDO} python ${DIR}/Sublist3r/setup.py install 2>/dev/null
+${SMK_SUDO} python3 ${DIR}/Sublist3r/setup.py install 2>/dev/null
+echo -e "\033[0;0m[\033[0;32m❯\033[0;0m] $2 Installation done";
