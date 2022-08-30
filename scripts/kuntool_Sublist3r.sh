d94745beb9e40371761ba9e00f93651838fc5a83
⛩️kun <82044074+root-tanishq@users.noreply.github.com>
2022-08-30 21:09:07 +0530

    Update kuntool_Sublist3r.sh

--- scripts/kuntool_Sublist3r.sh
+++ scripts/kuntool_Sublist3r.sh
@@ -2,7 +2,8 @@
 # URL to Repository => https://github.com/aboul3la/Sublist3r
 # Author => Ahmed Aboul-Ela
 ${SMK_SUDO} apt install git python python3 python-pip python3-pip -y 2>/dev/null
-git clone https://github.com/aboul3la/Sublist3r.git ${DIR}/Sublist3r
+cd ${DIR}
+git clone https://github.com/aboul3la/Sublist3r.git
 ${SMK_SUDO} python -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
 ${SMK_SUDO} python3 -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
 ${SMK_SUDO} python ${DIR}/Sublist3r/setup.py install 2>/dev/null
