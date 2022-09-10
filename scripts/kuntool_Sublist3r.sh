2766cc0c1e6ddbd1efeb717de250fbd558c16199
⛩️kun <82044074+root-tanishq@users.noreply.github.com>
2022-09-11 02:28:40 +0530

    Update kuntool_Sublist3r.sh

--- scripts/kuntool_Sublist3r.sh
+++ scripts/kuntool_Sublist3r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # URL to Repository => https://github.com/aboul3la/Sublist3r
 # Author => Ahmed Aboul-Ela
-${SMK_SUDO} apt install git python python3 python-pip python3-pip -y 2>/dev/null
+${SMK_SUDO} apt install git -y 2>/dev/null
 cd ${DIR}
 # Cloning Repository
 git clone https://github.com/aboul3la/Sublist3r.git
@@ -10,10 +10,13 @@ function tool_install() {
 ${SMK_SUDO} ${PYTHON_PATH} -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
 ${SMK_SUDO} ${PYTHON_PATH} ${DIR}/Sublist3r/setup.py install 2>/dev/null
 }
-if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = ]];then
+if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" ]];then
   export PYTHON_PATH=$(which python);
   tool_install
-elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = ]];then
+elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" ]];then
   export PYTHON_PATH=$(which python3);
   tool_install
+else 
+  ${SMK_SUDO} apt install python python-pip -y 2>/dev/null
+  ${SMK_SUDO} apt install python3 python3-pip -y 2>/dev/null
 fi
