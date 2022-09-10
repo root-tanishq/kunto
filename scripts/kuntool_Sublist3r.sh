6b43ea856446906478aa4d929e6c8ac1268f39ad
⛩️kun <82044074+root-tanishq@users.noreply.github.com>
2022-09-11 02:48:32 +0530

    Update kuntool_Sublist3r.sh

--- scripts/kuntool_Sublist3r.sh
+++ scripts/kuntool_Sublist3r.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 # URL to Repository => https://github.com/aboul3la/Sublist3r
 # Author => Ahmed Aboul-Ela
-${SMK_SUDO} apt install git -y 2>/dev/null
+${SMK_SUDO} apt install git -y
 cd ${DIR}
 # Cloning Repository
 git clone https://github.com/aboul3la/Sublist3r.git
 # Main tool installation
 function tool_install() {
-${SMK_SUDO} ${PYTHON_PATH} -m pip install -r ${DIR}/Sublist3r/requirements.txt 2>/dev/null
-${SMK_SUDO} ${PYTHON_PATH} ${DIR}/Sublist3r/setup.py install 2>/dev/null
+${SMK_SUDO} ${PYTHON_PATH} -m pip install -r ${DIR}/Sublist3r/requirements.txt
+${SMK_SUDO} ${PYTHON_PATH} ${DIR}/Sublist3r/setup.py install
 }
-if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" ]];then
+if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
   export PYTHON_PATH=$(which python);
   tool_install
-elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" ]];then
+elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
   export PYTHON_PATH=$(which python3);
   tool_install
-else 
-  ${SMK_SUDO} apt install python python-pip -y 2>/dev/null
-  ${SMK_SUDO} apt install python3 python3-pip -y 2>/dev/null
+else
+  ${SMK_SUDO} apt install python python-pip -y
+  ${SMK_SUDO} apt install python3 python3-pip -y
+  if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
+    export PYTHON_PATH=$(which python);
+    tool_install
+  elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
+    export PYTHON_PATH=$(which python3);
+    tool_install
+  else
+    echo "Issue in python"
+  fi
 fi
