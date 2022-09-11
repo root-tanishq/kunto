a2dd96c1891324a72487217433cfa738fc6c8412
root-tanishq <tanishqanimations@gmail.com>
2022-09-11 13:52:05 +0530

    Sunday 11 September 2022 01:52:05 PM IST

---
+++ scripts/kuntool_paramspider.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# URL to Repository => https://github.com/aboul3la/Sublist3r
+# Author => Ahmed Aboul-Ela
+if [[ ! -e $(which git) ]];then
+  ${SMK_SUDO} apt install git -y
+fi
+cd ${DIR}
+# Cloning Repository
+git clone https://github.com/devanshbatham/ParamSpider
+cd ParamSpider
+# Main tool installation
+function tool_install() {
+${SMK_SUDO} ${PYTHON_PATH} -m pip install -r ${DIR}/ParamSpider/requirements.txt
+}
+if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
+  export PYTHON_PATH=$(which python);
+  tool_install
+elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
+  export PYTHON_PATH=$(which python3);
+  tool_install
+else
+  ${SMK_SUDO} apt install python python-pip -y
+  ${SMK_SUDO} apt install python3 python3-pip -y
+  if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
+    export PYTHON_PATH=$(which python);
+    tool_install
+  elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
+    export PYTHON_PATH=$(which python3);
+    tool_install
+  else
+    echo "Issue in python"
+  fi
+fi
