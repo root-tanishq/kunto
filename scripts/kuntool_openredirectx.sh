9099f15170b7b24413691ac222ded90a2d1794da
root-tanishq <tanishqanimations@gmail.com>
2022-09-11 13:55:46 +0530

    Sunday 11 September 2022 01:55:46 PM IST

--- scripts/kuntool_openredirectx.sh
+++ scripts/kuntool_openredirectx.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
-# URL to Repository => https://github.com/aboul3la/Sublist3r
-# Author => Ahmed Aboul-Ela
+# URL to Repository => https://github.com/devanshbatham/OpenRedireX
+# Author => devanshbatham
 if [[ ! -e $(which git) ]];then
   ${SMK_SUDO} apt install git -y
 fi
 cd ${DIR}
 # Cloning Repository
-git clone https://github.com/devanshbatham/ParamSpider
-cd ParamSpider
+git clone https://github.com/devanshbatham/OpenRedireX
+cd OpenRedireX
 # Main tool installation
-function tool_install() {
-${SMK_SUDO} ${PYTHON_PATH} -m pip install -r ${DIR}/ParamSpider/requirements.txt
-${SMK_SUDO} ln -s ${DIR}/ParamSpider/paramspider.py /usr/local/bin/paramspider
-${SMK_SUDO} chmod +x /usr/local/bin/paramspider
-}
-if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
-  export PYTHON_PATH=$(which python);
-  tool_install
-elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
-  export PYTHON_PATH=$(which python3);
-  tool_install
-else
-  ${SMK_SUDO} apt install python python-pip -y
-  ${SMK_SUDO} apt install python3 python3-pip -y
-  if [[ -e $(which python) && $(python --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip) ]];then
-    export PYTHON_PATH=$(which python);
-    tool_install
-  elif [[ -e $(which python3) && $(python3 --version | awk '{print $2}' | awk -F'.' '{print $1}') = "3" && -e $(which pip3) ]];then
-    export PYTHON_PATH=$(which python3);
-    tool_install
-  else
-    echo "Issue in python"
-  fi
-fi
+${SMK_SUDO} ln -s ${DIR}/OpenRedireX/openredirex.py /usr/local/bin/openredirex
+${SMK_SUDO} chmod +x /usr/local/bin/openredirex
